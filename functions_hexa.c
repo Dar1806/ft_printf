e7da52c605e29cd574e8d76112421216f0484a89
nmeunier <noemeuniercoli@gmail.com>
2025-11-26 23:48:14 +0100

    final

--- functions_hexa.c
+++ functions_hexa.c
@@ -12,13 +12,13 @@
 
 #include "ft_printf.h"
 
-int	ft_putnbrhexa_low_ul(unsigned long n)
+int	ft_putnbrhexa_low_long(unsigned long n)
 {
 	int	count;
 
 	count = 0;
 	if (n >= 16)
-		count += ft_putnbrhexa_low_ul(n / 16);
+		count += ft_putnbrhexa_low_long(n / 16);
 	count += ft_putchar("0123456789abcdef"[n % 16]);
 	return (count);
 }
@@ -52,5 +52,5 @@ int	ft_voidhexa(void *adress)
 	if (adress == 0)
 		return (write(1, "(nil)", 5));
 	value = (unsigned long)adress;
-	return (write(1, "0x", 2) + ft_putnbrhexa_low_ul(value));
+	return (write(1, "0x", 2) + ft_putnbrhexa_low_long(value));
 }
